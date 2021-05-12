cd462ff200e240639a7a957babf2fc1d9713360c
Davide Gioia <61879923+DavideGioia@users.noreply.github.com>
2021-05-12 11:15:00 +0200

    Delete footer.php

--- template/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-    <div class="content has-text-centered">
-        <p><strong><?php echo $website_name ?></strong> Sviluppata da <a href="#">Davide Gioia</a></p>
-    </div>
-</footer>
