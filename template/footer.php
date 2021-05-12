b6ce12b8da19e837482b5a042d8bc93099921ab8
Davide Gioia <61879923+DavideGioia@users.noreply.github.com>
2021-05-12 11:13:56 +0200

    Add files via upload

---
+++ template/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <div class="content has-text-centered">
+        <p><strong><?php echo $website_name ?></strong> Sviluppata da <a href="#">Davide Gioia</a></p>
+    </div>
+</footer>
---
+++ template/header.php
@@ -0,0 +1,30 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="container">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="http://localhost/ELABORATO/index.php">
+                <strong><?php echo $website_name ?></strong>
+            </a>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="http://localhost/ELABORATO/index.php">Home</a>
+                <a class="navbar-item" href="http://localhost/ELABORATO/events.php">Eventi</a>
+            </div>
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary" href="http://localhost/ELABORATO/register.php">
+                            <strong>Registrati</strong>
+                        </a>
+                        <a class="button is-light" href="http://localhost/ELABORATO/login.php">Accedi</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
